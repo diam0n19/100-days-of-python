81daa4a7e1926e4f13262f2f9992c301fc29ed75
Alan Liu <diamonalan1@gmail.com>
2024-02-17 15:44:44 -0500

    section_1

---
+++ section_1.py
@@ -0,0 +1,40 @@
+# print ("she said "hello" and left")   #can't use double quotes within double quotes
+# print ('she said "hello" and left')   #can either use a single quote outside and double quote inside or wise versa
+# print ("she said \"hello\" and left") #or we can use '\'
+
+# print ("hello world!\nMy Name is\nBaboon sucks at teaching")     # '\n' allows us to type on a new line
+
+#print ("Baboon" + " is ape")  # we can add two strings together using '+'
+
+#input("how old is baboon?") #'input' prompts the user for a result
+
+#print ("Baboon is " + input("how old is baboon?"))
+
+#print (len(input("what is your name?")))  #(len()) calculates the amount of characters within a string
+
+# name = input("what is your name?")
+# print (name)
+
+# word = input("give me a word: ")
+
+# length = (len(word))
+
+# print ("your word " + (word) + " have " + str(length) +" letters")
+
+# a = input()
+# b = input()
+
+# c = a
+# a = b
+# b = c
+
+# print ("a: " + a)
+# print ("b: " + b)
+
+# city = input("What city did you grow up in?")
+
+# pet = input("what did you name your pet growing up?")
+
+# print ("Your band name would be " + city + " " + pet)
+
+
