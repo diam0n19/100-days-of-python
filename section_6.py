5c8170fecda44295d94dc065ca74dab1d96658cc
Alan Liu <diamonalan1@gmail.com>
2024-02-29 15:49:32 -0500

    day 6

---
+++ section_6.py
@@ -0,0 +1,18 @@
+# def robotmove():
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     robotmove()
+#     turn_up()
+#     turn_up()
+#     move()
+
+# for moves in range(6):   #-------> this tells how many turns loops
+#     jump()
+
+# number_of_moves = 6
+# while number_of_moves > 0:           #while loop
+#     jump()
+#     number_of_moves -=1
+
